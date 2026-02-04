4f0c6d3a43222f4ca866fa03d0a083ba5cbcb72c
anuraag <anuraagkumar7678@gmail.com>
2026-02-04 15:12:30 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import { connect, Provider } from "react-redux";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="color ">Tic Tac Toe</h1>
+      <Board></Board>
     </div>
+  );
+}
+
+function Board() {
+  const [marks, setMarks] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  const [player, setPlayer] = useState(1);
+
+  useEffect(() => {
+    const combinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    let winner = 0;
+    for (let c of combinations) {
+      if (marks[c[0]] === 1 && marks[c[1]] === 1 && marks[c[2]] === 1) {
+        winner = 1;
+        alert(`Player ${winner} wins!`);
+        setMarks([0, 0, 0, 0, 0, 0, 0, 0, 0]);
+      }
+      if (marks[c[0]] === 2 && marks[c[1]] === 2 && marks[c[2]] === 2) {
+        winner = 2;
+        alert(`Player ${winner} wins!`);
+        setMarks([0, 0, 0, 0, 0, 0, 0, 0, 0]);
+      }
+    }
+  }, [marks]);
+
+  const changeMark = (i) => {
+    const m = [...marks];
+    if (m[i] === 0) {
+      m[i] = player;
+      setMarks(m);
+      setPlayer(player === 1 ? 2 : 1);
+    } else {
+      alert("please click on empty block");
+    }
+  };
+
+  return (
+    <div className="Board">
+      <div>
+        <Block mark={marks[0]} position={0} changeMark={changeMark}></Block>
+        <Block mark={marks[2]} position={2} changeMark={changeMark}></Block>
+        <Block mark={marks[1]} position={1} changeMark={changeMark}></Block>
+      </div>
+      <div>
+        <Block mark={marks[3]} position={3} changeMark={changeMark}></Block>
+        <Block mark={marks[4]} position={4} changeMark={changeMark}></Block>
+        <Block mark={marks[5]} position={5} changeMark={changeMark}></Block>
+      </div>
+      <div>
+        <Block mark={marks[6]} position={6} changeMark={changeMark}></Block>
+        <Block mark={marks[7]} position={7} changeMark={changeMark}></Block>
+        <Block mark={marks[8]} position={8} changeMark={changeMark}></Block>
+      </div>
+    </div>
+  );
+}
+function Block({ mark, changeMark, position }) {
+  return (
+    <div
+      className={`Block mark${mark}`}
+      onClick={(e) => changeMark(position)}
+    ></div>
   );
 }
 
